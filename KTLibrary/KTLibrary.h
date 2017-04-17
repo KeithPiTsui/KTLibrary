86e3fd12428fc90bdc454b1e2872c83e1b21eaa1
KeithPiTsui <85331524@qq.com>
2017-04-17 20:14:50 +0800

    Initial Commit

---
+++ KTLibrary/KTLibrary.h
@@ -0,0 +1,19 @@
+//
+//  KTLibrary.h
+//  KTLibrary
+//
+//  Created by Pi on 17/04/2017.
+//  Copyright © 2017 Keith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KTLibrary.
+FOUNDATION_EXPORT double KTLibraryVersionNumber;
+
+//! Project version string for KTLibrary.
+FOUNDATION_EXPORT const unsigned char KTLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KTLibrary/PublicHeader.h>
+
+
